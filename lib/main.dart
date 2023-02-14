006d4cdc57e2705f07cc74eacf243041f9afcdb8
Abdullah Ayaz <106861398+abd529@users.noreply.github.com>
2023-02-14 15:35:09 +0500

    removed problems from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+// ignore_for_file: unused_import
+
 import 'package:climate_care/home.dart';
 import 'package:climate_care/login_screens/login.dart';
 import 'package:climate_care/onboarding/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,10 +19,10 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color.fromRGBO(71, 191, 83, 1.0),
         appBarTheme: AppBarTheme(backgroundColor: Colors.green, elevation: 0, titleTextStyle: GoogleFonts.poppins(fontSize: 18))
       ),
-      home: Home(),
+      home: const Home(),
       routes: {
-        LoginScreen.routeName: (ctx) => LoginScreen(),
-        Home.routeName: (ctx) => Home(),
+        LoginScreen.routeName: (ctx) => const LoginScreen(),
+        Home.routeName: (ctx) => const Home(),
       },
     );
   }
